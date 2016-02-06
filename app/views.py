6971b9d572f7b80425cd10ed43af8fe1f26ca884
Kim Do Hyun <nero.union12@gmail.com>
2016-02-07 02:35:03 +0900

    Done add_post event with ModelForm with multiselect

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    redirect,
+)
+from django.http import HttpResponse
 from .forms import ModelFormExample
+from .models import Tag
 
 
 def index(req):
+    if req.method == "POST":
+        form = ModelFormExample(req.POST, req)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = req.user
+            post.save()
+            for tag_pk in req.POST.getlist('tag'):
+                post.tag.add(Tag.objects.get(pk=tag_pk))
+            post.save()
+            return HttpResponse("Post is added successful")
     form = ModelFormExample()
     return render(req, 'index.html', {'form': form})
