a83e7a1107ae656a2732b467674445fbd488b154
Kim Do Hyun <nero.union12@gmail.com>
2016-02-06 18:54:44 +0900

    Add models like blog service

--- app/models.py
+++ app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=64)
+
+    def __str__(self):
+        return '{} {}'.format(self.__class__.__name__, self.title)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=64)
+
+    def __str__(self):
+        return '{} {}'.format(self.__class__.__name__, self.title)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name='posts')
+    category = models.ForeignKey(Category, related_name='posts')
+    tag = models.ManyToManyField(Tag, related_name='posts')
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+def __str__(self):
+    return '{} {}'.format(self.__class__.__name__, self.title)
+
+
+class Comment(models.Model):
+    parent_post = models.ForeignKey(Post, related_name='comments')
+    author = models.ForeignKey(User, related_name='comments')
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.__class__.__name__, self.title)
